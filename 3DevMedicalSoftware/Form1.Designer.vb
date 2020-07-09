d9599e4cde0647792026125956b19855a4023b7d
dmrf1980 <you@example.com>
2020-07-09 19:28:17 -0300

    Agregar archivos de proyecto.

---
+++ 3DevMedicalSoftware/Form1.Designer.vb
@@ -0,0 +1,76 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.PanelPrincipal = New System.Windows.Forms.Panel()
+        Me.BarraSuperiorVerde = New System.Windows.Forms.Panel()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.PanelPrincipal.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'PanelPrincipal
+        '
+        Me.PanelPrincipal.BackColor = System.Drawing.Color.FromArgb(CType(CType(228, Byte), Integer), CType(CType(228, Byte), Integer), CType(CType(226, Byte), Integer))
+        Me.PanelPrincipal.Controls.Add(Me.Panel1)
+        Me.PanelPrincipal.Controls.Add(Me.BarraSuperiorVerde)
+        Me.PanelPrincipal.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.PanelPrincipal.Location = New System.Drawing.Point(0, 0)
+        Me.PanelPrincipal.Name = "PanelPrincipal"
+        Me.PanelPrincipal.Size = New System.Drawing.Size(1008, 729)
+        Me.PanelPrincipal.TabIndex = 0
+        '
+        'BarraSuperiorVerde
+        '
+        Me.BarraSuperiorVerde.BackColor = System.Drawing.Color.FromArgb(CType(CType(65, Byte), Integer), CType(CType(113, Byte), Integer), CType(CType(111, Byte), Integer))
+        Me.BarraSuperiorVerde.Dock = System.Windows.Forms.DockStyle.Top
+        Me.BarraSuperiorVerde.Location = New System.Drawing.Point(0, 0)
+        Me.BarraSuperiorVerde.Name = "BarraSuperiorVerde"
+        Me.BarraSuperiorVerde.Size = New System.Drawing.Size(1008, 41)
+        Me.BarraSuperiorVerde.TabIndex = 0
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.FromArgb(CType(CType(83, Byte), Integer), CType(CType(92, Byte), Integer), CType(CType(92, Byte), Integer))
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Left
+        Me.Panel1.Location = New System.Drawing.Point(0, 41)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(200, 688)
+        Me.Panel1.TabIndex = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1008, 729)
+        Me.Controls.Add(Me.PanelPrincipal)
+        Me.Name = "Form1"
+        Me.Text = "Medical Software"
+        Me.PanelPrincipal.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents PanelPrincipal As Panel
+    Friend WithEvents BarraSuperiorVerde As Panel
+    Friend WithEvents Panel1 As Panel
+End Class
